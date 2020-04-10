96fb6c3c7fa88151ad6f4b71d17e68b9d4439592
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-04-10 07:54:51 -0500

    add pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// create variables to store questions
+
+// use mouse click to start quiz
+
+// write for loops to cycle through quiz questions
+
+// use key press events to receive user input in form of answers to quiz questions
+
+// create time limit using functions
+
+// use conditional statements to determine wrong or right answers
+
+// when question is answered wrong, time is subtracted from the clock
+
+// when all questions are answered or timer reaches 0, game is over
+
+// use client storage to store high scores
